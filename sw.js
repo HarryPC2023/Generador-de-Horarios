faeea97199feed6ff7db203f1abacff12d2bc57d
Harry <harrypc2021@hotmail.com>
2026-07-29 09:51:05 -0500

    mejorar diseño

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // sw.js — Service Worker
 // Permite instalación como PWA y funcionamiento offline
 // ============================================================
-const CACHE_NAME = 'horariogen-v31';
+const CACHE_NAME = 'horariogen-v32';
  
 // Lista de archivos que se guardan en caché al instalar la app
 const ARCHIVOS_CACHE = [
